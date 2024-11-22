89cc8e7b91d9ffe257c99591ede166d391b06ff9
Zarkec <2656207830@qq.com>
2024-11-22 23:29:54 +0800

    修正音乐信息数据库表中的字段名称

--- melody_db.sql
+++ melody_db.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80036
  File Encoding         : 65001
 
- Date: 18/11/2024 13:55:21
+ Date: 22/11/2024 23:29:21
 */
 
 SET NAMES utf8mb4;
@@ -24,16 +24,16 @@ DROP TABLE IF EXISTS `music_infos`;
 CREATE TABLE `music_infos`  (
   `music_id` int NOT NULL AUTO_INCREMENT,
   `music_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `music_anthor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `music_author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `music_picurl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `music_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`music_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of music_infos
 -- ----------------------------
 INSERT INTO `music_infos` VALUES (1, 'Don’t say “lazy”', '放課後ティータイム', 'D:/cppCode/last/Melody/res/music/Don\'t say lazy.jpg', 'D:/cppCode/last/Melody/res/music/Don\'t say lazy.mp3');
-INSERT INTO `music_infos` VALUES (2, 'ギターと孤独と蒼い惑星', '結束バンド', 'D:/cppCode/last/Melody/res/music/ギターと孤独と蒼い惑星 - 結束バンド.jpg', 'D:/cppCode/last/Melody/res/music/ギターと孤独と蒼い惑星 - 結束バンド.mp3');
+INSERT INTO `music_infos` VALUES (2, 'ギターと孤独と蒼い惑星', '結束バンド', 'D:/cppCode/last/Melody/res/music/ギターと孤独と蒼い惑星.jpg', 'D:/cppCode/last/Melody/res/music/ギターと孤独と蒼い惑星.mp3');
 
 SET FOREIGN_KEY_CHECKS = 1;
